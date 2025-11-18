f705a79db72918d1fae146fc98c7228b392af8a4
Anton Strömberg <anton@kruso.se>
2025-11-18 09:42:47 +0100

    Inital commit

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import { useStoryblokApi } from '@storyblok/astro'
+
+export async function getLinks() {
+  const storyblokApi = useStoryblokApi()
+  console.log("hello")
+  try {
+    const links = await storyblokApi.getAll('cdn/links', {
+        //version: isPreview() ? 'draft' : 'published',
+      version: 'draft'
+    })
+    return links
+  } catch (error) {
+    console.error(`Error fetching links:`, error)
+    return null
+  }
+}
---
+++ src/utils/generateStaticPaths.js
@@ -0,0 +1,19 @@
+import { getLinks } from '../utils/api';
+
+const links = await getLinks();
+
+export default async function generateStaticPaths() {
+	return links
+		.filter((link) => !link.is_folder)
+		.map((link) => {
+			//This slug will be used for fetching data from storyblok
+			const slug = link.slug === 'home' ? undefined : link.slug;
+			
+			return {
+				props: { slug },
+				params: {
+					slug: slug,
+				},
+			};
+		});
+}
