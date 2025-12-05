23b16551b26f51578a2de4f69eafb657161f9df4
Anton Strömberg <anton@kruso.se>
2025-12-05 15:12:24 +0100

    Remove redundant check

--- src/utils/storyblokEditable.ts
+++ src/utils/storyblokEditable.ts
@@ -6,7 +6,5 @@ import {
 export function storyblokEditable<T extends Record<string, any>>(
 	blok: T,
 ): Record<string, any> {
-	if (import.meta.env.DEV)
-		return _storyblokEditable(blok as unknown as SbBlokData);
-	return {};
+	return _storyblokEditable(blok as unknown as SbBlokData);
 }
